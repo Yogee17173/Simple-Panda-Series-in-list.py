bcb40460bb2ec5b168edf5710eb0d2b772b7fb11
YOGEESH K P <yogeesh793@gmail.com>
2025-02-17 23:27:54 +0530

    Add files via upload

---
+++ Simple Panda Series in list.py
@@ -0,0 +1,16 @@
+# Create a simple Pandas Series from a list
+import pandas as pd
+a=[1,7,2]
+myvar=pd.Series(a)
+print(myvar) 
+
+import pandas as kp
+j=[1,2,3,4,5,6]
+myvar=kp.Series(j)
+print(myvar)
+
+import pandas as re
+k=[5,6,4,7,8,5,2,3,4,8,6,4,1,2]
+l=re.Series(k)
+print(l)
+
